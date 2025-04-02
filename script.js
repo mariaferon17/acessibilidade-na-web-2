a6ce4ac2305f36b150cd5d733275e8d7e80e4400
mariaferon17 <maria.feron@escola.pr.gov.br>
2025-04-02 13:54:37 +0000

    oi

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded' function(){
+    const aumentafontebotao document.getElementById('aumentar-font');
+    const diminuiFonteBotao document.getElementById ('diminui-fonte');
+
+    let tamanhoaAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoaAtualFonte += 0.1;
+        document.body.style.fontsize = '${tamanhoAtualFonte}rem'
+   
+    })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoaAtualFonte -= 0.1;
+        document.body.style.fontsize = '${tamanhoAtualFonte}rem'
+        
+    })
+})
+        
+    
